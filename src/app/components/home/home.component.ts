af721042d199ec23433c9137040c66b7f745582b
HAMDI <benyaflahhamdi2@gmail.com>
2018-10-18 22:10:17 +0100

    navbar/footer/contact/home//register/login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  titles = ['HOT SALE', 'TOP RATED', 'POPULAR'];
+
+  lastRowHomePage = [
+    [
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-31.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-32.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-33.jpg'}
+    ],
+    [
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-34.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-35.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-36.jpg'}
+    ], [
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-37.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-38.jpg'},
+      {category: 'WIRELESS', productName: 'productName', price: 98, img: 'product-39.jpg'}
+    ]
+  ];
+
+  popularProducts = [
+    {category: 'SMART WATCHES', productName: 'SMARTWATCH 3 SWR50', price: 630, img: 'product-120.jpg', description: 'description'},
+    {category: 'SMART WATCHES', productName: 'SMARTWATCH 3 SWR50', price: 630, img: 'product-119.jpg', description: 'description'},
+    {category: 'SMART WATCHES', productName: 'SMARTWATCH 3 SWR50', price: 630, img: 'product-118.jpg', description: 'description'},
+    {category: 'SMART WATCHES', productName: 'SMARTWATCH 3 SWR50', price: 630, img: 'product-117.jpg', description: 'description'}
+  ];
+
+  countDown = ['days', 'hours', 'minutes', 'seconds'];
+
+  bestProposes = [
+    {category: 'Modern Edition', productName: 'Modern beat ht', img: 'product-115.jpg', description: 'description'},
+    {category: 'Modern Edition', productName: 'Modern beat ht', img: 'product-116.jpg', description: 'description'}
+  ];
+
+  firstRowHomePage = [
+    {solde: 'Modern Edition', promotion: 'Modern beat ht', img: 'product-130.jpg', description: 'description'},
+    {solde: 'Modern Edition', promotion: 'Modern beat ht', img: 'product-131.jpg', description: 'description'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
