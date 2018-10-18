bb99e6e39cebadc3a6019588db8a541b8ce48d44
HAMDI <benyaflahhamdi2@gmail.com>
2018-10-18 16:10:37 +0100

    tarki7 jaw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+
+const appRoutes: Routes = [
+
+  { path: 'not-found', component: ErrorComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ErrorComponent,
+    ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
