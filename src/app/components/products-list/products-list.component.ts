93260ac51e1f3dd491c383cfd29ca1d5c8e4b9df
SAID Rahma <saaidrahma@gmail.com>
2018-10-19 10:14:37 +0100

    numb1

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+//import * as data from 'src/assets/contenu.json' ;
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
-
+  name : string;
+  description: string ;
+  price : string;
+  src: string;
   constructor() { }
 
   ngOnInit() {
