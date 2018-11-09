dfd8592fc95fa48306ac13444a22d3af812c4a33
HAMDI <benyaflahhamdi2@gmail.com>
2018-11-09 18:40:12 +0100

    productDetails

--- src/app/components/products-list/product-details/product-details.component.ts
+++ src/app/components/products-list/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../../../assets/contenu.json';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,7 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  description: string;
+  price: string;
+  category: string;
+  src: string;
+  width;
+  height;
+  case;
+  depth;
+  material;
+  available;
+  views;
+
+  getDetails(id) {
+    const details = data['products'].filter(detail => {
+      if (detail.id === Number(id)) {
+        this.name = detail.name;
+        this.price = detail.price;
+        this.description = detail.description;
+        this.src = detail.src;
+        this.category = detail.category;
+
+        this.width = detail.width;
+        this.height = detail.height;
+        this.case = detail.case;
+        this.depth = detail.depth;
+        this.material = detail.material;
+        this.available = detail.available;
+        this.views = detail.views;
+      }
+    });
+  }
+
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      console.log(this.getDetails(params['id']));
+    });
+  }
 
   ngOnInit() {
   }
